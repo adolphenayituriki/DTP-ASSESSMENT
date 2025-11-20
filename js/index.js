a0961720c559aecae3a4aae15b736aa6efddc7f7
Adolphe NAYITURIKI <www.nayituriki.com@gmail.com>
2025-11-20 13:01:50 +0200

    indexx.js added some functions to make real time

--- js/index.js
+++ js/index.js
@@ -4,3 +4,45 @@ document.getElementById("issueForm").addEventListener("submit", function(e){
     // later: send via fetch() to PHP API
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Load issues when the modal is opened
+      const viewModal = document.getElementById("viewIssueModal");
+      viewModal.addEventListener("show.bs.modal", loadIssues);
+
+});
+
+function loadIssues() {
+      fetch("api/get_issues.php")
+            .then(response => response.json())
+            .then(data => {
+                  const tableBody = document.querySelector("#issuesTable tbody");
+                  tableBody.innerHTML = "";
+
+                  data.forEach(issue => {
+                  tableBody.innerHTML += `
+                        <tr>
+                              <td>${issue.id}</td>
+                              <td>${issue.title}</td>
+                              <td>${issue.description}</td>
+                              <td>${issue.reporter}</td>
+                              <td>${issue.created_at}</td>
+                        </tr>
+                  `;
+            });
+      })
+      .catch(error => console.error("Error loading issues:", error));
+}
+
+
+function updateDateTime(){
+      let now = new Date();
+      let time = now.toLocaleTimeString();
+      let year = now.toTimeString();
+      document.getElementById("time").textContent = "("+year+")";
+      document.getElementById("time").style.color = "#0995a5ff"
+}
+setInterval(updateDateTime, 1000);
+updateDateTime();
+
+
